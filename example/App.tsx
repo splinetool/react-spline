c653a0bc373586ff9ae818ecddd0311350829c00
Claudia Benito <claudiabdm@hotmail.es>
2022-03-09 19:24:58 +0100

    fix: clean example (#19)

--- example/App.tsx
+++ example/App.tsx
@@ -6,130 +6,63 @@ import anime from 'animejs';
 function App() {
   const splineRef = useRef<SplineRef>(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [isOn, setIsOn] = useState(true);
 
-  const lightRef = useRef<SPEObject>();
-
+  const tileRef = useRef<SPEObject>();
   useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
-    function handleKeyDown(e: KeyboardEvent) {
-      if (e.key === 'a') {
-        setIsOn((current) => !current);
-      }
-    }
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
+    const tile = splineRef.current?.findObjectById(
+      '7ba78968-2a55-48f2-b14c-5191da3e075e'
+    );
+    tileRef.current = tile;
+  }, [splineRef.current]);
 
-  useEffect(() => {
-    const light = splineRef.current?.findObjectByName('Directional Light');
-    lightRef.current = light;
-  }, [splineRef.current?.findObjectByName]);
-
-  useEffect(() => {
-    if (isOn) {
-      splineRef.current?.emitEventReverse(
-        'mouseDown',
-        '96E5E232-68FD-474E-9F9C-85EF9F8A354A'
-      );
-      anime({
-        targets: lightRef.current,
-        intensity: 0.8,
-        duration: 500,
-      });
-
-      document.documentElement.style.setProperty(
-        '--background',
-        'hsl(229.6, 57.8%, 82.4%)'
-      );
-    } else {
-      splineRef.current?.emitEvent(
-        'mouseDown',
-        '96E5E232-68FD-474E-9F9C-85EF9F8A354A'
-      );
-
-      anime({
-        targets: lightRef.current,
-        intensity: 0,
-        duration: 500,
-      });
-
-      document.documentElement.style.setProperty(
-        '--background',
-        'hsl(229.6, 10.8%, 5.4%)'
-      );
-    }
-  }, [isOn]);
-
-  function handleMouseDown(e: SplineEvent) {
-    if (e.target.name === 'Switch') {
-      setIsOn((current) => !current);
-    }
-  }
-
-  function handleMoveLight(direction: 'left' | 'right' | 'up' | 'down') {
-    if (lightRef.current) {
+  function handleMoveTile(direction: 'left' | 'right' | 'up' | 'down') {
+    if (tileRef.current) {
+      const newPosition = { ...tileRef.current.position };
       switch (direction) {
         case 'left':
-          lightRef.current.position.x -= 100;
+          newPosition.x -= 100;
           break;
         case 'right':
-          lightRef.current.position.x += 100;
+          newPosition.x += 100;
           break;
         case 'up':
-          lightRef.current.position.y -= 100;
+          newPosition.y += 100;
           break;
         case 'down':
-          lightRef.current.position.y += 100;
+          newPosition.y -= 100;
           break;
       }
+      anime({
+        targets: tileRef.current.position,
+        ...newPosition,
+        duration: 500,
+      });
     }
   }
 
   return (
     <>
-      <div
-        style={{
-          maxWidth: '512px',
-          margin: 'auto',
-        }}
-      >
-        {/* 
-        Commented this because example scene needs to be .spline and I only have the .json for now.
-        {isLoading && (
-          <div style={{ width: '512px', height: '512px' }}>Loading...</div>
-        )}
-        <Spline
-          ref={splineRef}
-          scene="https://draft-dev.spline.design/2Uw-TdfHVr2SkSYA/scene.json"
-          id="myCanvas"
-          onMouseDown={handleMouseDown}
-          onLoad={() => setIsLoading(false)}
-        />
-        <button type="button" onClick={() => setIsOn((current) => !current)}>
-          Switch {isOn ? 'off' : 'on'}
-        </button>{' '}
-        or Press 'A' keyword
-        <div>
-          <button type="button" onClick={() => handleMoveLight('left')}>
-            Move light to left
-          </button>
-          <button type="button" onClick={() => handleMoveLight('right')}>
-            Move light to right
-          </button>
-          <button type="button" onClick={() => handleMoveLight('up')}>
-            Move light up
-          </button>
-          <button type="button" onClick={() => handleMoveLight('down')}>
-            Move light down
-          </button>
-        </div> */}
+      <div className='buttons'>
+        <button type="button" onClick={() => handleMoveTile('left')}>
+          Move tile to left
+        </button>
+        <button type="button" onClick={() => handleMoveTile('right')}>
+          Move tile to right
+        </button>
+        <button type="button" onClick={() => handleMoveTile('up')}>
+          Move tile up
+        </button>
+        <button type="button" onClick={() => handleMoveTile('down')}>
+          Move tile down
+        </button>
       </div>
-      {/* Working fine with local file (./scene.spline) which was downloaded from web content but not from url */}
       <Spline
-        scene="https://draft-dev.spline.design/1vYXL8gMu7ZEiUzs/scene.spline"
-        id="myCanvas2"
+        ref={splineRef}
+        autoRender
+        scene="https://prod.spline.design/ft9KFAMYebCiRXbC/scene.spline"
+        onLoad={() => {
+          setIsLoading(false);
+        }}
       />
     </>
   );
