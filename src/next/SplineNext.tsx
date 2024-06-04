2501594e1f4d548a59c4f0facd9ffd5db900b465
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2024-06-04 15:46:01 +0200

    fix: get from backend

--- src/next/SplineNext.tsx
+++ src/next/SplineNext.tsx
@@ -2,19 +2,37 @@ import { decodePreview } from './decodePreview';
 import Image from 'next/image';
 import Spline, { SplineProps } from '../Spline';
 
-export default function SplineNext({
-  placeholder,
+async function getPreview(scene: string) {
+  const matches =
+    /^https:\/\/([^\/]+).spline.design\/([^\/]+)\/scene.splinecode/gi.exec(
+      scene
+    );
+  let placeholder: string | undefined = undefined;
+  let img: string | undefined = undefined;
+  if (matches?.[2]) {
+    const scope = matches[1]; // prod or draft
+    const id = matches[2];
+    try {
+      const res = await fetch(`https://${scope}.spline.design/${id}/hash`);
+      placeholder = await res.text();
+      img = decodePreview(placeholder);
+    } catch (e) {}
+  }
+  return { placeholder, img };
+}
+
+export default async function SplineNext({
   width,
   height,
   ...props
 }: SplineProps) {
-  const img = placeholder && decodePreview(placeholder);
+  const data = await getPreview(props.scene);
   return (
     <>
-      {img && (
+      {data.img && (
         <Image
-          src={img}
-          id={placeholder}
+          src={data.img}
+          id={data.placeholder}
           alt="Spline preview"
           style={{
             width: width ? width + 'px' : '100%',
@@ -24,7 +42,7 @@ export default function SplineNext({
           height={height ?? 100}
         />
       )}
-      <Spline placeholder={placeholder} {...props} />
+      <Spline placeholder={data.placeholder} {...props} />
     </>
   );
 }
